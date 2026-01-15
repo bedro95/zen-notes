999c05f9ede43a9652a020b63a6982e3796f5240
Bader <elkorgliplay@gmail.com>
2026-01-15 02:41:22 +0200

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'ring': 'ring 2s ease-in-out infinite',
+      },
+      keyframes: {
+        ring: {
+          '0%, 100%': { transform: 'rotate(0deg)' },
+          '10%, 30%, 50%, 70%, 90%': { transform: 'rotate(10deg)' },
+          '20%, 40%, 60%, 80%': { transform: 'rotate(-10deg)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
